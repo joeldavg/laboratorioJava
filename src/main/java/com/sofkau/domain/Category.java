be40f6bd4b706bebe091953abd828e600efb15f2
Joel Guzman <joel-guzman@hotmail.com>
2022-04-22 13:53:12 -0500

    Create domain classes --- Category and Player classes were created

--- src/main/java/com/sofkau/domain/Category.java
+++ src/main/java/com/sofkau/domain/Category.java
@@ -1,4 +1,38 @@
 package com.sofkau.domain;
 
 public class Category {
+
+    private Long id;
+    private Level nivel;
+    private Long scoreId;
+
+    public Category(Long id, Level nivel, Long scoreId) {
+        this.id = id;
+        this.nivel = nivel;
+        this.scoreId = scoreId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Level getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(Level nivel) {
+        this.nivel = nivel;
+    }
+
+    public Long getScoreId() {
+        return scoreId;
+    }
+
+    public void setScoreId(Long scoreId) {
+        this.scoreId = scoreId;
+    }
 }
