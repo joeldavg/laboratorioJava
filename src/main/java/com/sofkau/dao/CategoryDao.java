27d3ee85188df24ffd78e86f8a9d4e0cec8926eb
Joel Guzman <joel-guzman@hotmail.com>
2022-04-22 14:16:27 -0500

    REFACTOR --- CategoryDao was refactored

--- src/main/java/com/sofkau/dao/CategoryDao.java
+++ src/main/java/com/sofkau/dao/CategoryDao.java
@@ -5,6 +5,7 @@ import com.sofkau.domain.Level;
 import com.sofkau.exception.GenericException;
 
 public interface CategoryDao {
+
     public Category getByLevel(Level level) throws GenericException;
 
 }
