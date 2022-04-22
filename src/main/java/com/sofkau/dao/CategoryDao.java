d66212a90fd0f1f9fae60dad2fc6153ff6af7a37
Joel Guzman <joel-guzman@hotmail.com>
2022-04-22 14:15:03 -0500

    ADD --- created GenericException and modified CategoryDao interface

--- src/main/java/com/sofkau/dao/CategoryDao.java
+++ src/main/java/com/sofkau/dao/CategoryDao.java
@@ -1,7 +1,10 @@
 package com.sofkau.dao;
 
+import com.sofkau.domain.Category;
+import com.sofkau.domain.Level;
+import com.sofkau.exception.GenericException;
+
 public interface CategoryDao {
-
-
+    public Category getByLevel(Level level) throws GenericException;
 
 }
