b47df72270d40394e6b54fa45014fbad53539118
David <depch47@gmail.com>
2022-04-23 20:08:45 -0500

    ADD--- GameDaoImpl

--- src/main/java/com/sofkau/dao/GameDao.java
+++ src/main/java/com/sofkau/dao/GameDao.java
@@ -6,6 +6,6 @@ import com.sofkau.exception.GenericException;
 import java.util.List;
 
 public interface GameDao {
-    public List<Game> getByJugadoresId(Long playerId) throws GenericException;
+    public List<Game> getByPlayerId(Long playerId) throws GenericException;
     public Game save(Game game) throws GenericException;
 }
